42fa1f6bfe9e7138ecf9bd6e7c56921797cee179
John Mark Redding <johomurk@gmail.com>
2018-12-28 13:24:55 -0600

    Finish Assoc

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 User.destroy_all
 Band.destroy_all
 
-user = User.new(name: 'John')
+user = User.new(name: 'John', username: "johnny", password: "nice")
 user.save
 user = User.find_by_name('John')
 
-user_band = user.bands.new(name: 'Band #1', description: 'My first band')
+user_band = Band.new(name: 'Band #1', description: 'My first band', manager_id: user.id)
 
 if user_band.save
   puts "Done"
